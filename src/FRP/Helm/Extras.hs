466122748cb658700bcd9633a3a83a6707453f41
niil <niil.94@hotmail.com>
2014-12-18 23:17:22 +0100

    Add wrap function

--- src/FRP/Helm/Extras.hs
+++ src/FRP/Helm/Extras.hs
@@ -1,8 +1,21 @@
 module FRP.Helm.Extras
     ( near
-    , onScreen
+    , clamp
+    , wrap
     )where
 
 {-| Check if two values are within a tolerance -}
 near :: (Num a, Ord a) => a -> a -> a -> Bool
 near tolerance x y = abs (x - y) <= tolerance
+
+{-| "Clamps" the third argument within given range. -}
+clamp :: Ord a => a -> a -> a -> a
+clamp a b = min b . max a
+
+{-| Wraps the third argument within given range. -}
+wrap :: (Ord a, Num a) => a -> a -> a -> a
+wrap a b c
+    | a <= c && c < b = c
+    | c <= a     = wrap a b (c + b - a)
+    | otherwise = wrap a b (c - b - a)
+
