8a42f5ac9c3724ed5ee3bb670aa6968f619cf7ff
niil <niil.94@hotmail.com>
2014-12-18 13:03:28 +0100

    Add onScreen

--- src/FRP/Helm/Extras.hs
+++ src/FRP/Helm/Extras.hs
@@ -1,4 +1,16 @@
+module FRP.Helm.Extras
+    ( near
+    , onScreen
+    )where
 
-{-| Checks if two values are within a tolerance -}
-near :: Double -> Double -> Double -> Bool
+{-| Check if two values are within a tolerance -}
+near :: (Num a, Ord a) => a -> a -> a -> Bool
 near tolerance x y = abs (x - y) <= tolerance
+
+{-| Check if a point is on the screen -}
+onScreen :: (Fractional a, Ord a) => (a, a) -> (Int, Int) -> Bool
+onScreen (x, y) (w, h) = (-w' / 2) < x && x < (w' / 2) &&
+                         (-h' / 2) < y && y < (h' / 2)
+                            where
+                                w' = fromIntegral w
+                                h' = fromIntegral h
